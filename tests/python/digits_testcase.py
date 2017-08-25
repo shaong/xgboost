1f8683641dbc1261e2825c864c3b733a42b6e0d3
Chong Shao Hong <cshaong@outlook.com>
2017-08-26 00:44:00 +0800

    fixed unit test

--- tests/python/digits_testcase.py
+++ tests/python/digits_testcase.py
@@ -3,18 +3,19 @@ import itertools
 import json
 import numpy as np
 import xgboost as xgb
-from sklearn.datasets import load_digit
+from sklearn.datasets import load_digits
+from sklearn.model_selection import KFold
 
 class TestDigits(unittest.TestCase):
     digits = load_digits(2)
     y = digits['target']
     X = digits['data']
 
-    kf = KFold(n_splits=2, shuffle=True, random_state=rng)
+    kf = KFold(n_splits=2, shuffle=True)
     for train_index, test_index in kf.split(X):
         xgb_model = xgb.XGBClassifier().fit(X[train_index],y[train_index])
-        predictions = xgb_model.predict(X[test_index])
-        actuals = y[test_index]
-        print(confusion_matrix(actuals, predictions))
-
-    assert (prediction-actuals) == 0 
+        prediction = xgb_model.predict(X[test_index])
+        actual = y[test_index]
+    print(prediction)
+    print(actual)
+    assert prediction.all() == actual.all()
